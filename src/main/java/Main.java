e9a3076a39a1bcbf61debe0db2c8842acd8e0ead
Thomas S. Sylvest <tss@it-minds.dk>
2015-01-13 13:09:30 +0100

    Implemented timer feature...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        final int loops = 10;
+        final Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < loops; i++) {
+                        System.out.println("Time now: " + System.currentTimeMillis());
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
     }
 
 }
